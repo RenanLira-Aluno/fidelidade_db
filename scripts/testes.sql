b4a62287885ed8215a14d8a9a730ecfbcef3b284
Renan-Lira <renan.almeida@lojaspintos.com.br>
2025-07-02 10:36:06 -0300

    novos testes

--- scripts/testes.sql
+++ scripts/testes.sql
@@ -1,7 +1,21 @@
 
-select * from venda;
+SELECT * FROM cliente;
 
 SELECT fechar_venda(2);
 
 
-SELECT * FROM cliente;
+SELECT * FROM cupons_disponiveis_por_cliente WHERE id_cliente = 1 and id_cupom = 1;
+
+
+-- Teste de venda
+SELECT * FROM venda NATURAL JOIN item_venda WHERE id_venda = 6;
+
+
+SELECT abrir_venda('123.456.789-01');
+SELECT * FROM produto;
+SELECT registrar_item_venda(6, 'PRATO002', 2);
+SELECT registrar_item_venda(6, 'BEB002', 2);
+SELECT registrar_item_venda(6, 'BEB001', 2);
+
+SELECT fechar_venda(6);
+
