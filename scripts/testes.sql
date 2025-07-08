86e49db49db29476d2d7016c2b14e284c98dc046
Thiago Pessoa <tpessoa96@gmail.com>
2025-07-07 22:50:20 -0300

    Testando inserções deleções e atualizações e deixando pronto alguns exemplos

--- scripts/testes.sql
+++ scripts/testes.sql
@@ -59,3 +59,32 @@ SELECT
 
 SELECT
     fechar_venda (7);
+
+
+    select * from cliente
+select * from venda
+select * from item_venda
+select * from cupom
+select * from resgate_cupom
+select * from categoria_programa
+select * from produto
+select cadastrar_produto('CAMISA001','Camisa casual', 'Camisa casual',20.00,'Roupas', 50)
+select cadastrar_categoria_programa(7,'Radiante', 8000)
+select atualizar_categoria_programa(7, 'Radiante', 1000)
+select excluir_categoria_programa(7)
+select cadastrar_cliente('Thiago','000.000.000-00','Thiago@email.com','86988244789')
+select atualizar_dados_cliente(1,'','Renan@email.com','86988218047')
+select abrir_venda('123.456.789-01')
+select abrir_venda('123.456.789-01')
+select registrar_item_venda(5, 'PRATO002', 10)
+select fechar_venda(5)
+select resgatar_cupom('123.456.789-01',7)
+select cadastrar_cupom(1,'Cupom 50%', 'Desconto 50%', 600, 60)
+select excluir_cupom(18)
+select atualizar_dados_cupom(17,null,'Cupom atualizado','',null,null,null)
+select aplicar_cupom_venda('7', '090e0cf3', '1')
+
+SELECT setval(
+  pg_get_serial_sequence('produto', 'id_produto'),
+  (SELECT MAX(id_produto) FROM produto)
+);
